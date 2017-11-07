c6d99fec03988b19cb6426ca72b7aabbf46b589f
gabmac <30659678+gabmac@users.noreply.github.com>
2017-11-07 00:48:20 -0200

    Create Exp1a.vhd

---
+++ lab1/Exp1a.vhd
@@ -0,0 +1,13 @@
+LIBRARY ieee;
+
+USE ieee.std_logic_1164.all;
+
+ENTITY Exp1a IS
+	PORT ( SW : IN STD_LOGIC_VECTOR(17 DOWNTO 0);
+	LEDR : OUT STD_LOGIC_VECTOR(17 DOWNTO 0));
+END Exp1a;
+
+ARCHITECTURE Behavior OF Exp1a IS
+BEGIN
+	LEDR <= SW;
+END Behavior;
